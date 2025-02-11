2c2c87830d34f9285314549145a15e4fbef54c33
Christos Kechagias <ck.kechagias@gmail.com>
2025-02-11 21:04:01 +0200

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-vue-app/', // Replace 'my-vue-app' with your repository name
+  base: '/stellar-blue/',
   plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
